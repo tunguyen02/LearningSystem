fa4f2ce26ced50bcf505ee99e48991d1eda6ebe3
tunguyen02 <tu700131@gmail.com>
2024-10-27 22:44:08 +0700

    course and user

---
+++ backend/controllers/course.controller.js
@@ -0,0 +1,23 @@
+const courseController = {
+    getAllCourses: (req, res) => {
+
+    },
+
+    getCourseById: (req, res) => {
+
+    },
+
+    createCourse: (req, res) => {
+
+    },
+
+    updateCourse: (req, res) => {
+
+    },
+
+    deleteCourse: (req, res) => {
+
+    }
+}
+
+export default courseController;
---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,23 @@
+const userController = {
+    registerUser: (req, res) => {
+
+    },
+
+    loginUser: (req, res) => {
+
+    },
+
+    logoutUser: (req, res) => {
+
+    },
+
+    getUser: (req, res) => {
+
+    },
+
+    updateUser: (req, res) => {
+
+    },
+}
+
+export default userController;
