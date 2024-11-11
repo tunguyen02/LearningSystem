c8601b2f0531d6f0d91349a4e9c3a42bad0a8991
tunguyen02 <tu700131@gmail.com>
2024-11-11 22:15:14 +0700

    user router

--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -5,5 +5,7 @@ const UserRouter = Router();
 
 UserRouter.post('/register', userController.register);
 UserRouter.post('/login', userController.logIn);
+UserRouter.get('/', userController.getAllUsers);
+UserRouter.get('/:id', userController.getUserById);
 
 export default UserRouter;
