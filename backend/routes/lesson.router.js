767fb6de1b299bc60656e6c2a7475c602c6e7f2b
tunguyen02 <tu700131@gmail.com>
2024-11-13 22:47:27 +0700

    modify lesson and register-course

--- backend/routes/lesson.router.js
+++ backend/routes/lesson.router.js
@@ -3,9 +3,9 @@ import lessonController from "../controllers/lesson.controller.js";
 
 const LessonRouter = Router();
 
-LessonRouter.get('/', lessonController.getAllLessons);
-LessonRouter.get('/:id', lessonController.getLessonById);
-LessonRouter.post('/', lessonController.createLesson);
+LessonRouter.get('/all', lessonController.getAllLessons);
+LessonRouter.get('/detail/:id', lessonController.getLessonById);
+LessonRouter.post('/create', lessonController.createLesson);
 LessonRouter.put('/:id', lessonController.updateLesson);
 LessonRouter.delete('/:id', lessonController.deleteLesson);
 
--- backend/routes/registerCourse.router.js
+++ backend/routes/registerCourse.router.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import registerCourseController from "../controllers/registerCourse.controller.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const RegistrationRouter = Router();
 
-RegistrationRouter.get('/', registerCourseController.getAllRegistrations);
 RegistrationRouter.get('/new', registerCourseController.getNewRegistrations);
-RegistrationRouter.post('/', registerCourseController.registerCourse);
-RegistrationRouter.put('/:id', registerCourseController.updateRegistrationStatus);
+RegistrationRouter.get('/', registerCourseController.getAllRegistrations);
+RegistrationRouter.post('/', authMiddleware, registerCourseController.registerCourse);
+RegistrationRouter.put('/:id', authMiddleware, registerCourseController.updateRegistrationStatus);
 
 export default RegistrationRouter;
