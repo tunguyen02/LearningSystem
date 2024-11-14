a23261cc46111545c8f83d6b6b990296060381ba
tunguyen02 <tu700131@gmail.com>
2024-11-14 22:32:41 +0700

    checkAdmin

--- backend/routes/course.router.js
+++ backend/routes/course.router.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import courseController from '../controllers/course.controller.js';
-import { authMiddleware } from '../middleware/authMiddleware.js';
+import { authMiddleware, checkAdmin } from '../middleware/authMiddleware.js';
 
 const CourseRouter = Router();
 const upload = multer({ storage: multer.memoryStorage() });
@@ -9,9 +9,9 @@ const upload = multer({ storage: multer.memoryStorage() });
 CourseRouter.get('/my-courses', authMiddleware, courseController.getAllCourseByUser);
 CourseRouter.get('/', courseController.getAllCourses);
 CourseRouter.get('/:id', courseController.getCourseById);
-CourseRouter.post('/', authMiddleware, upload.single('image'), courseController.createCourse);
-CourseRouter.put('/:id', authMiddleware, courseController.updateCourse);
-CourseRouter.delete('/:id', authMiddleware, courseController.deleteCourse);
+CourseRouter.post('/', authMiddleware, checkAdmin, upload.single('image'), courseController.createCourse);
+CourseRouter.put('/:id', authMiddleware, checkAdmin, courseController.updateCourse);
+CourseRouter.delete('/:id', authMiddleware, checkAdmin, courseController.deleteCourse);
 CourseRouter.get('/:id/confirm', authMiddleware, courseController.isCourseConfirmed);
 
 export default CourseRouter;
