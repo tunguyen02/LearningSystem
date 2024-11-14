27478f38c1c0d40cae8981c990597c9b4b63de51
tunguyen02 <tu700131@gmail.com>
2024-11-14 22:32:41 +0700

    checkAdmin

--- backend/routes/lesson.router.js
+++ backend/routes/lesson.router.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import lessonController from "../controllers/lesson.controller.js";
-import { authMiddleware } from "../middleware/authMiddleware.js";
+import { authMiddleware, checkAdmin } from "../middleware/authMiddleware.js";
 
 const LessonRouter = Router();
 
 LessonRouter.get('/all', lessonController.getAllLessons);
 LessonRouter.get('/detail/:id', lessonController.getLessonById);
-LessonRouter.post('/create', authMiddleware, lessonController.createLesson);
-LessonRouter.put('/:id', authMiddleware, lessonController.updateLesson);
-LessonRouter.delete('/:id', authMiddleware, lessonController.deleteLesson);
+LessonRouter.post('/create', authMiddleware, checkAdmin, lessonController.createLesson);
+LessonRouter.put('/:id', authMiddleware, checkAdmin, lessonController.updateLesson);
+LessonRouter.delete('/:id', authMiddleware, checkAdmin, lessonController.deleteLesson);
 
 export default LessonRouter;
