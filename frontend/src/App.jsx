dd71f1e93113febb721a51c320eb033a46db0298
Truong Phan <137706113+truong-pc@users.noreply.github.com>
2024-11-24 13:22:06 +0700

    layout for User

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import {Navigate,Route,Routes} from "react-router-dom";
+import "./App.css";
+import User from "./layout/User";
+//import Courses from "./pages/user/courses/Courses.jsx";
+
+const App = () => {
+    return (
+        <Routes>
+            <Route path="/user" element={<User />}>
+                {/* <Route path="courses" element={<Courses />} /> */}
+            </Route>
+
+            <Route path="/" element={<Navigate to="/user" />} />
+        </Routes>
+    );
+    };
+
+export default App;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import App from "./App.jsx";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>
+);
