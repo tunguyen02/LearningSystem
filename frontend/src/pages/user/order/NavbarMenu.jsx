359c4dadd40a5ed3a3e1429d023808c4f9b7fd20
Truong Phan <137706113+truong-pc@users.noreply.github.com>
2024-11-27 00:45:13 +0700

    Layout and Navbar for course-order

---
+++ frontend/src/pages/user/order/NavbarMenu.jsx
@@ -0,0 +1,59 @@
+import {
+    IoCheckmarkCircle,
+    IoCloseCircle,
+    IoTimeOutline,
+  } from "react-icons/io5";
+  import { NavLink } from "react-router-dom";
+  
+  const data = [
+    {
+      icon: <IoCheckmarkCircle size={25} />,
+      title: "Approved",
+      path: "/user/course-order/approved",
+    },
+    {
+      icon: <IoCloseCircle size={25} />,
+      title: "Cancelled",
+      path: "/user/course-order/cancelled",
+    },
+    {
+      icon: <IoTimeOutline size={25} />,
+      title: "Pending",
+      path: "/user/course-order/pending",
+    },
+  ];
+
+ const NavbarMenu = () => {
+    return (<div className="w-full border-b border-b-cyan-100">
+        <div className="flex gap-2 w-full">
+          {data.map((item, index) => (
+            <NavLink
+              to={item.path}
+              key={index}
+              className="relative flex items-center gap-2 pb-2 flex-1 justify-center cursor-pointer text-gray-600 transition-colors duration-300"
+            >
+              {({ isActive }) => (
+                <>
+                  <div
+                    className={`flex items-center gap-2 ${
+                      isActive ? "text-cyan-500" : "hover:text-cyan-500"
+                    }`}
+                  >
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </div>
+                  <span
+                    className={`absolute bottom-0 left-0 w-full h-[2px] bg-cyan-500 transform transition-transform duration-300 ${
+                      isActive ? "scale-x-100" : "scale-x-0"
+                    }`}
+                  ></span>
+                </>
+              )}
+            </NavLink>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+export default NavbarMenu;
