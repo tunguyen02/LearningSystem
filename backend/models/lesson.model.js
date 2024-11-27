b44dd89208ad963cd633fdc71055aa16d22ff25d
Trần Tuấn Tú <155437883+TuanTu1@users.noreply.github.com>
2024-11-27 21:44:33 +0700

    Adding Lesson Model

---
+++ backend/models/lesson.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const lessonSchema = new mongoose.Schema({
+    courseId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+        required: true,
+    },
+    order: {
+        type: Number,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    videos: [
+        {
+            title: {
+                type: String,
+                // required: true,
+            },
+            url: {
+                type: String,
+                // required: true,
+            }
+        }
+    ]
+},
+    {
+        timestamps: true,
+    }
+);
+
+const LessonModel = mongoose.model("Lesson", lessonSchema);
+export default LessonModel;
