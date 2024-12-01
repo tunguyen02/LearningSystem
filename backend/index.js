5a25571ae6640adcccec8189fc5930164e754ba7
tunguyen02 <tu700131@gmail.com>
2024-12-01 20:18:04 +0700

    router

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,13 @@ import cors from 'cors';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import { RootRouterV1 } from './routes/index.js';
+import { createAdmin } from './scripts/createAdmin.js';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
-import { createAdmin } from './scripts/createAdmin.js';
 
 
 app.use((req, res, next) => {
