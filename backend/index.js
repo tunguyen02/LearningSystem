afbef8fb0b449d4d57c7444e54351f444a94e8f2
tunguyen02 <tu700131@gmail.com>
2024-12-01 20:17:28 +0700

    create admin reset

--- backend/index.js
+++ backend/index.js
@@ -10,17 +10,33 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
+import { createAdmin } from './scripts/createAdmin.js';
+
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 })
 
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to the API!');
+});
+
 app.use('/api/v1', RootRouterV1);
+
+app.use((req, res) => {
+    res.status(404).send({
+        status: 'fail',
+        message: 'Route not found',
+    });
+});
 
 
 mongoose.connect(process.env.DATABASE)
-    .then(() => console.log('Database connected successfully'))
+    .then(() => {
+        console.log('Database connected successfully');
+        createAdmin();
+    })
     .catch(err => console.log(err));
 
 
